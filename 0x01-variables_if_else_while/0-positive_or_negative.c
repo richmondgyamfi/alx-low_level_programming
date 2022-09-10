5b2a39854d524169997fbc95c86d712502ca2ccb
Richmond Gyamfi Nketia <rnketia25@gmail.com>
2022-09-10 10:21:08 -0700

    mynit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int r;
+
+	if (r > 0)
+		printf("%d is positive\n", r);
+	else
+		printf("%d is negative\n", r);
+	return (0);
+}
