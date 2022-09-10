1726b0e98d0f77a4891c6107c32497deda52ad23
Richmond Gyamfi Nketia <rnketia25@gmail.com>
2022-09-10 12:35:14 -0700

    myconn

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 48;
+
+	while (a < 58)
+	{
+		putchar(a);
+		if (a != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
