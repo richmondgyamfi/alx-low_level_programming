4dfca4cbb46d47b55a84321e275dd3960eceff42
Richmond Gyamfi Nketia <rnketia25@gmail.com>
2022-09-15 04:17:23 -0700

    mysal

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+int _puchar(int c);
+
+#endif /* _SAMPLE_HEADER_H_ */
