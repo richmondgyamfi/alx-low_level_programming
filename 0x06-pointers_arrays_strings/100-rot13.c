4bcf319a722e8b0c209819d21ecf14e43cfe14c1
Richmond Gyamfi Nketia <rnketia25@gmail.com>
2022-09-22 08:51:25 -0700

    myghit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* rot13 - encodes a string into rot13
+* @s: string to be encoded
+*
+* Return: returns encoded string
+*/
+char *rot13(char *s)
+{
+	int i = 0, j;
+
+	char let[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char let13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (*(s + i) != '\0')
+	{
+		for (j = 0; j < 53; j++)
+		{
+			if (let[j] == s[i])
+			{
+				s[i] = let13[j];
+				break;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
